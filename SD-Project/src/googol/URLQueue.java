1ae5998ec6b00596f98459ee13d92c1f70507cb5
BernardoPedro <bernardopedro@student.dei.uc.pt>
2024-03-29 19:07:10 +0000

    Fila S2

--- SD-Project/src/googol/URLQueue.java
+++ SD-Project/src/googol/URLQueue.java
@@ -1,23 +1,31 @@
 package googol;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.rmi.*;
 
-public class URLQueue {
+public class URLQueue extends UnicastRemoteObject implements QueueInterface {
     private ConcurrentLinkedQueue<String> queue;
+    private String name;
 
-    public URLQueue() {
+    public URLQueue(String name) throws RemoteException {
+        super();
         this.queue = new ConcurrentLinkedQueue<>();
+        this.name = name;
+        System.out.println("URLQueue " + name + " created");
     }
 
-    public void enqueue(String url) {
+    public synchronized void enqueue(String url) throws RemoteException {
+        System.out.println("Adding to URLQueue " + url);
         queue.add(url);
     }
 
-    public String dequeue() {
+    public synchronized String dequeue() throws RemoteException {
         return queue.poll();
     }
 
-    public boolean isEmpty() {
+    public synchronized boolean isEmpty() throws RemoteException {
         return queue.isEmpty();
     }
 }
