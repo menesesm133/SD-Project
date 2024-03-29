92bb3fb1eea3cfe94f3679a722a930d9ad33b77c
BernardoPedro <bernardopedro@student.dei.uc.pt>
2024-03-29 19:39:06 +0000

    hashset na queue

--- SD-Project/src/googol/QueueInterface.java
+++ SD-Project/src/googol/QueueInterface.java
@@ -5,6 +5,8 @@ import java.rmi.*;
 public interface QueueInterface extends Remote {
     public void enqueue(String url) throws RemoteException;
 
+    public void notvisited(String url) throws RemoteException;
+
     public String dequeue() throws RemoteException;
 
     public boolean isEmpty() throws RemoteException;
--- SD-Project/src/googol/URLQueue.java
+++ SD-Project/src/googol/URLQueue.java
@@ -4,10 +4,12 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.rmi.*;
+import java.util.HashSet;
 
 public class URLQueue extends UnicastRemoteObject implements QueueInterface {
     private ConcurrentLinkedQueue<String> queue;
     private String name;
+    private HashSet<String> visitedurls = new HashSet<String>();
 
     public URLQueue(String name) throws RemoteException {
         super();
@@ -17,8 +19,18 @@ public class URLQueue extends UnicastRemoteObject implements QueueInterface {
     }
 
     public synchronized void enqueue(String url) throws RemoteException {
+
+        if (visitedurls.contains(url)) {
+            System.out.println("URL already visited: " + url);
+            return;
+        }
         System.out.println("Adding to URLQueue " + url);
+        visitedurls.add(url);
         queue.add(url);
+    }
+
+    public synchronized void notvisited(String url) throws RemoteException {
+        visitedurls.remove(url);
     }
 
     public synchronized String dequeue() throws RemoteException {
@@ -28,4 +40,5 @@ public class URLQueue extends UnicastRemoteObject implements QueueInterface {
     public synchronized boolean isEmpty() throws RemoteException {
         return queue.isEmpty();
     }
+
 }
